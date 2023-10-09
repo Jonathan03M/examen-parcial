3082321e0184f2d72d628f5bec83c0ef8fa11d39
Jonathan <yoninplay03@hotmail.com>
2023-10-09 15:53:37 -0300

    finalizado

---
+++ server/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/temperatura', async (req, res) => {
+  const ciudad = req.query.ciudad;
+  if (!ciudad) {
+    return res.status(400).json({ error: 'Debes proporcionar una ciudad.' });
+  }
+
+  try {
+    const apiKey = '98a574920f69651fe40076744fd98ed7'; // Reemplaza con tu clave de API
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}`;
+    const response = await axios.get(url);
+    const temperatura = response.data.main.temp;
+
+    res.json({ temperatura });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al obtener la temperatura.' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Aplicación escuchando en el puerto ${port}`);
+});
